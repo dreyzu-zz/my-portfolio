02e36a7fdf7c99af2c15944cb5bcc4b3f12ff3ee
dreyzu <61318133+dreyzu@users.noreply.github.com>
2021-07-13 12:42:43 +0200

    add Head tag from next and change shortcut icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import * as React from "react";
 import { ChakraProvider, VStack } from "@chakra-ui/react";
 import { theme } from "../theme";
+import Head from "next/head";
 
 function HomePage({ Component, pageProps }) {
     return (
         <ChakraProvider theme={theme}>
+            <Head>
+                <title>Tristan Derez</title>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+                <link rel="shortcut icon" href="/favicon.svg" />
+            </Head>
             <Component {...pageProps} />
         </ChakraProvider>
     );
